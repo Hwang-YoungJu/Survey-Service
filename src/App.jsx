ba933b62be0680db5502fb52211fb559b0cc3914
huey12312 <huey2269@gmail.com>
2022-03-30 13:55:20 +0900

    <EDIT> Readme.md

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import MainPage from "./pages/MainPage";
 
-const Wrapper = styled.div`
-  background-color: royalblue;
-  margin: 0 auto;
-  width: 100%;
-  height: 500px;
-`;
+
 
 function App() {
   return (
